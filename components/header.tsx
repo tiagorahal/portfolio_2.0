98997df43f97e0a88bd72e09619630227e462ad8
tiagorahal <rahal.aires@gmail.com>
2023-10-09 14:13:49 -0300

    Adjust background, Add header bar

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='z-[999] relative'>
+      <div className='fixed top-0 w-full left-1/2 -translate-x-1/2 h-[4.5rem] rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full md:w-[75%] lg:w-[50%]'>
+
+      </div>
+    </header>
+  )
+}
