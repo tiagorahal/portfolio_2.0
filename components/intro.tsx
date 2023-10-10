77c0a1baaefbd480252f175f3c3b2187ef7c48e0
tiagorahal <rahal.aires@gmail.com>
2023-10-09 23:06:52 -0300

    Add animation to paragraph

--- components/intro.tsx
+++ components/intro.tsx
@@ -45,13 +45,17 @@ export default function Intro() {
         </div>
       </div>
 
-      <h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
+      <motion.h1
+      className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      >
         <span className="font-bold">Hello, I'm Tiago.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">5 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React</span> and <span className="underline">Ruby on Rails</span>.
-      </h1>
+      </motion.h1>
     </section>
   );
 }
