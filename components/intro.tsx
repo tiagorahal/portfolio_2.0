f3acac26ae7f6384264f99a928d34e44e48e2003
tiagorahal <rahal.aires@gmail.com>
2023-10-09 23:52:25 -0300

    Fix identation issues

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 import profilePic from "../public/profile.jpeg";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs"
-import { HiDownload } from "react-icons/hi"
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
@@ -50,48 +50,54 @@ export default function Intro() {
       </div>
 
       <motion.h1
-      className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Tiago.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">5 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React</span> and <span className="underline">Ruby on Rails</span>.
+        <span className="underline">React</span> and{" "}
+        <span className="underline">Ruby on Rails</span>.
       </motion.h1>
 
       <motion.div
-      className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg"
-      initial={{ opacity: 0, y: 100}}
-      animate={{ opacity: 1, y: 0}}
-      transition={{
-        delay: 0.1
-      }}>
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
         <Link
-        href="#contact"
-        className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
           Contact me here
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-        href="/CV.pdf"
-        download
-        className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10">
+          href="/CV.pdf"
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+        >
           Download CV
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
         <a
-        href="https://www.linkedin.com/in/tiagorahal/"
-        target="_blank"
-        className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10">
+          href="https://www.linkedin.com/in/tiagorahal/"
+          target="_blank"
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+        >
           <BsLinkedin />
         </a>
         <a
-        href="https://github.com/tiagorahal"
-        target="_blank"
-        className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10">
-          <FaGithubSquare/>
+          href="https://github.com/tiagorahal"
+          target="_blank"
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <FaGithubSquare />
         </a>
       </motion.div>
     </section>
