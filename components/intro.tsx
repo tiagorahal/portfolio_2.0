d9b81a76b27abc8394a4ef21ba9b6d03e1034c1b
tiagorahal <rahal.aires@gmail.com>
2023-10-09 23:51:48 -0300

    Add animation to the links, insert valid paths on the links

--- components/intro.tsx
+++ components/intro.tsx
@@ -61,24 +61,39 @@ export default function Intro() {
         <span className="underline">React</span> and <span className="underline">Ruby on Rails</span>.
       </motion.h1>
 
-      <div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg">
+      <motion.div
+      className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg"
+      initial={{ opacity: 0, y: 100}}
+      animate={{ opacity: 1, y: 0}}
+      transition={{
+        delay: 0.1
+      }}>
         <Link
         href="#contact"
-        className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full">
+        className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
           Contact me here
-          <BsArrowRight />
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
         </Link>
-        <a className="bg-white px-7 py-3 flex items-center gap-2 rounded-full">
+        <a
+        href="/CV.pdf"
+        download
+        className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10">
           Download CV
-          <HiDownload />
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
-        <a className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem]">
+        <a
+        href="https://www.linkedin.com/in/tiagorahal/"
+        target="_blank"
+        className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10">
           <BsLinkedin />
         </a>
-        <a className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem]">
+        <a
+        href="https://github.com/tiagorahal"
+        target="_blank"
+        className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10">
           <FaGithubSquare/>
         </a>
-      </div>
+      </motion.div>
     </section>
   );
 }
