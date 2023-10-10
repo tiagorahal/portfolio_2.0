63ca661cdff1b9f4e1ededeb0c8c27b337470d46
tiagorahal <rahal.aires@gmail.com>
2023-10-10 14:29:27 -0300

    Fix linter issues

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,13 +8,11 @@ export default function Projects() {
     <section className="scroll-mt-28 mb-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
-        {
-          projectsData.map((project, index) => (
-            <React.Fragment key={index}>
-              <Project {...project} />
-            </React.Fragment>
-          ))
-        }
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
       </div>
     </section>
   );
