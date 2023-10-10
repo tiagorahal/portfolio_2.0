fe03d61252e6fabd4be4c999a7fd976f0675c950
tiagorahal <rahal.aires@gmail.com>
2023-10-10 11:14:53 -0300

    Add section divider fully animated

---
+++ components/section-divider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from 'react'
+import { motion } from 'framer-motion'
+
+export default function SectionDivider() {
+  return (
+    <motion.div
+    className='bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block'
+    initial={{ opacity: 0, y: 100}}
+    animate={{ opacity: 1, y: 0}}
+    transition={{
+      type: "spring",
+      stiffness: 125,
+      delay: 0.125,
+      duration: 0.7,
+    }}>
+    </motion.div>
+  )
+}
