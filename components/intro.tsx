e0126b06b0244ee44eadadce765111a510312045
tiagorahal <rahal.aires@gmail.com>
2023-10-13 12:21:10 -0300

    Set scroll to smooth on entire page

--- components/intro.tsx
+++ components/intro.tsx
@@ -13,7 +13,7 @@ export default function Intro() {
   return (
     <section
       id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-28"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
       <div className="flex items-center justify-center">
         <div className="relative">
--- components/projects.tsx
+++ components/projects.tsx
@@ -5,7 +5,7 @@ import Project from "./project";
 
 export default function Projects() {
   return (
-    <section className="scroll-mt-28 mb-28">
+    <section id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
