f9d4b43f4861c40bde2fa831ae02b933b319e5b2
tiagorahal <rahal.aires@gmail.com>
2023-10-14 09:14:49 -0300

    Add react-chrono

--- components/experience.tsx
+++ components/experience.tsx
@@ -2,57 +2,35 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
-import { useSectionInView } from "@/lib/hooks";
-import { useTheme } from "@/context/theme-context";
+import { Chrono } from "react-chrono";
+
+const items = [
+  {
+    title: "",
+    cardTitle: "Event 1",
+    cardSubtitle: "Event 1 Subtitle",
+    cardDetailedText: "This is the first event on the timeline.",
+  },
+  {
+    title: "",
+    cardTitle: "Event 2",
+    cardSubtitle: "Event 2 Subtitle",
+    cardDetailedText: "This is the second event on the timeline.",
+  },
+  {
+    title: "",
+    cardTitle: "Event 3",
+    cardSubtitle: "Event 3 Subtitle",
+    cardDetailedText: "This is the third event on the timeline.",
+  },
+];
 
 export default function Experience() {
-  const { ref } = useSectionInView("Experience");
-  const { theme } = useTheme();
-
   return (
-    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="">
-        {experiencesData.map((item, index) => (
-          <React.Fragment key={index}>
-            <VerticalTimelineElement
-              contentStyle={{
-                background:
-                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-                boxShadow: "none",
-                border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
-                padding: "1.3rem 2rem",
-              }}
-              contentArrowStyle={{
-                borderRight:
-                  theme === "light"
-                    ? "0.4rem solid #9ca3af"
-                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
-              }}
-              date={item.date}
-              icon={item.icon}
-              iconStyle={{
-                background:
-                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
-                fontSize: "1.5rem",
-              }}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                {item.description}
-              </p>
-            </VerticalTimelineElement>
-          </React.Fragment>
-        ))}
-      </VerticalTimeline>
+      <Chrono items={items} mode="VERTICAL_ALTERNATING" itemWidth={300} />
     </section>
   );
 }
--- lib/data.ts
+++ lib/data.ts
@@ -33,25 +33,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Lorem Ipsum",
-    location: "Lorem Ipsum",
-    description: "Lorem Ipsum",
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "Lorem Ipsum",
+    date: "2019",
   },
   {
-    title: "Lorem Ipsum",
-    location: "Lorem Ipsum",
-    description: "Lorem Ipsum",
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
     icon: React.createElement(CgWorkAlt),
-    date: "Lorem Ipsum",
+    date: "2019 - 2021",
   },
   {
-    title: "Lorem Ipsum",
-    location: "Lorem Ipsum",
-    description: "Lorem Ipsum",
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "Lorem Ipsum",
+    date: "2021 - present",
   },
 ] as const;
 
